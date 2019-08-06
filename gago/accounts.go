9af5fc8868f9316cff27a4329ccc83e275e2dde3
Mark Edmondson <github@markedmondson.me>
2019-08-06 19:07:18 +0200

    better account summary

--- gago/accounts.go
+++ gago/accounts.go
@@ -33,17 +33,15 @@ func GetAccountSummary(service *ga.Service) {
 	}
 
 	fmt.Println("Found following account summary:")
-	for _, ass := range accountSummaryResponse.Items {
-
-		fmt.Println("Account:", ass.Id, ass.Name)
-
+	for i, ass := range accountSummaryResponse.Items {
+		if i == 0 {
+			fmt.Println("accountId accountName webPropertyId webPropertyName viewId viewName")
+		}
 		for _, wp := range ass.WebProperties {
-
-			fmt.Println("WebProperty:", wp.Id, wp.Name)
 
 			for _, view := range wp.Profiles {
 
-				fmt.Println("ViewID:", view.Id, view.Name)
+				fmt.Println(ass.Id, ass.Name, wp.Id, wp.Name, view.Id, view.Name)
 			}
 
 		}
